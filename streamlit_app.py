505d793cef6a116e40cb28cd3e7287e017b342f9
iremsudogan <108758842+iremsudogan@users.noreply.github.com>
2024-05-31 13:38:41 +0300

    Update45 streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ class CustomLabelEncoder(BaseEstimator, TransformerMixin):
             X[col] = encoder.transform(X[col])
         return X
 
+
 class FeaturesAdder(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
